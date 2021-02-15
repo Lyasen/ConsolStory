5c3563c3e5e78d58464f06a7225bbabb2ccc1ace
Lyasen <lyasen.hq@gmx.fr>
2021-02-15 15:21:17 +0100

    My first commit

---
+++ src/main/java/fr/lyasen/consolstory/ConsolStoryApplication.java
@@ -0,0 +1,13 @@
+package fr.lyasen.consolstory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsolStoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsolStoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/lyasen/consolstory/ConsolStoryApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.lyasen.consolstory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsolStoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
